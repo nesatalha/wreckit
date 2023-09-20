60fff6d39a6b50a944b59c00e69231f92c53c9b8
nesatalha <nesatalha@gmail.com>
2023-09-21 01:10:42 +0300

    debug banner removed

--- lib/app.dart
+++ lib/app.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'posts/view/posts_page.dart';
 
 class App extends MaterialApp {
-  App({super.key}) : super(home: const PostsPage(),theme: ThemeData.dark());
+  App({super.key}) : super(home: const PostsPage(),theme: ThemeData.dark(), debugShowCheckedModeBanner: false);
 }
